e4a82d8f3d984e32b7f36965db66f377990d0d1c
MattBast <matthewbastow93@gmail.com>
2015-07-20 14:37:46 +0100

    help box functions have been moved to own file. help box added to patternCount box
    should change name of patternCount to summaryBox. Will do this later

--- app/js/startup.js
+++ app/js/startup.js
@@ -10,10 +10,6 @@ fileInput.addEventListener( "change", insertButton );
 var fileUploadButton = document.getElementById( "fileUploadButton" );
 fileUploadButton.addEventListener( "click", clickButton );
 
-//the help boxes
-var fileInputHelpBox = document.getElementById( "fileInputHelpBox" );
-var numberOfNodesHelpBox = document.getElementById( "numberOfNodesHelpBox" );
-
 //the variables controlling the loading spinner
 var c = document.getElementById( "canvas" );
 var ctx = c.getContext( "2d" );
@@ -77,36 +73,6 @@ function clickButton() {
 	else {
 		fadeOut( fileInputBox, numOfNodesBox );
 	}
-}
-
-function helpOpen( box ) {
-	box.style.display = "block";
-	var opacity = 0.1; //<-- initial opacity
-	var timer = setInterval( function() {
-		//stop increasing opacity when reach 1
-		if( opacity >= 1 ) {
-			clearInterval( timer );
-		}
-		//increase opacity
-		box.style.opacity = opacity;
-		box.style.filter = "alpha(opacity=" + opacity * 100 + ")";
-		opacity += opacity * 0.1;
-	}, 50 );
-}
-
-function helpClose( box ) {
-	var opacity = 1; //<-- initial opacity
-	var timer = setInterval( function() {
-		//stop decreasing opacity when reach 0.1
-		if( opacity <= 0.1 ) {
-			clearInterval( timer );
-			box.style.display = "none";
-		}
-		//decrease opacity
-		box.style.opacity = opacity;
-		box.style.filter = "alpha(opacity=" + opacity * 100 + ")";
-		opacity -= opacity * 0.1;
-	}, 50 );
 }
 
 //-------------------- loading spinner functions -------------------
